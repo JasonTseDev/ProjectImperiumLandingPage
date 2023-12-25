54286d7799812d05cbf03f799e02259f465d9587
JASON TSE <jasontse.dev@gmail.com>
2023-12-25 18:46:04 -0500

    trigger deploy

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
+
   const linkElement = screen.getByText(/Project Imperium: The Ultimate Zomboid Playground/i);
   expect(linkElement).toBeInTheDocument();
 });
